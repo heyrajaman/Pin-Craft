9e6b69a9a7ceaa8df1c5835790f098a9a2e71183
Aman Singh <saman31999@gmail.com>
2025-07-17 01:06:58 +0530

    create Routes, Auth Layout with Outlet, Card design

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -1,14 +1,56 @@
+import { Link } from "react-router";
+
 const GalleryItem = ({ item }) => {
   return (
     <div
-      className="flex relative"
+      className="flex relative group"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
-      <img
-        src={item.media}
-        alt=""
-        className="rounded-xl object-cover w-full"
+      <img src={item.media} alt="" className="rounded-xl object-cover w-full" />
+      <Link
+        to={`/pin/${item.id}`}
+        className="hidden group-hover:block
+          absolute inset-0
+          bg-black bg-opacity-30
+          rounded-[16px]"
       />
+      <button
+        className="hidden group-hover:block
+          bg-[#e50829] text-white
+          rounded-[20px]
+          py-1 px-3
+          font-medium cursor-pointer
+          w-max absolute top-4 right-4
+          border-none"
+      >
+        Save
+      </button>
+      <div
+        className="hidden group-hover:flex
+          absolute bottom-4 right-4
+          items-center gap-2"
+      >
+        <button
+          className="w-8 h-8
+            rounded-full
+            bg-white
+            flex items-center justify-center
+            border-none cursor-pointer
+            hover:bg-gray-100"
+        >
+          <img src="/general/share.svg" alt="" className="w-5 h-5" />
+        </button>
+        <button
+          className="w-8 h-8
+            rounded-full
+            bg-white
+            flex items-center justify-center
+            border-none cursor-pointer
+            hover:bg-gray-100"
+        >
+          <img src="/general/more.svg" alt="" className="w-5 h-5" />
+        </button>
+      </div>
     </div>
   );
 };
